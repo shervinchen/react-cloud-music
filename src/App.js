13ecfc0d6e3ea702766c3926fb1e1b41222a6447
csdoker <csdokerchen@yunify.com>
2021-03-03 13:42:55 +0800

    feat: 引入redux及immutable相关库

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react'
+
 import { renderRoutes } from 'react-router-config'
 import { HashRouter } from 'react-router-dom'
 import routes from './routes/index.js'
+
+import { Provider } from 'react-redux'
+import store from './store'
+
 import { GlobalStyle } from './style'
 import { IconStyle } from './assets/iconfont/iconfont'
 
 function App() {
   return (
-    <HashRouter>
-      <GlobalStyle></GlobalStyle>
-      <IconStyle></IconStyle>
-      {renderRoutes(routes)}
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <GlobalStyle></GlobalStyle>
+        <IconStyle></IconStyle>
+        {renderRoutes(routes)}
+      </HashRouter>
+    </Provider>
   )
 }
 
