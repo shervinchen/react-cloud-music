84018e8b2cd80c627e840881c73ebb1994371f93
csdoker <csdokerchen@yunify.com>
2021-03-03 15:35:14 +0800

    feat: 使用better-scroll封装通用scroll组件

--- src/application/Recommend/index.js
+++ src/application/Recommend/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Slider from '../../components/slider'
 import RecommendList from '../../components/list'
+import Scroll from '../../baseUI/scroll'
+import { Content } from './style'
 
 function Recommend() {
   //mock 数据
@@ -22,10 +24,14 @@ function Recommend() {
   })
 
   return (
-    <div>
-      <Slider bannerList={bannerList}></Slider>
-      <RecommendList recommendList={recommendList}></RecommendList>
-    </div>
+    <Content>
+      <Scroll className="list">
+        <div>
+          <Slider bannerList={bannerList}></Slider>
+          <RecommendList recommendList={recommendList}></RecommendList>
+        </div>
+      </Scroll>
+    </Content> 
   )
 }
 
