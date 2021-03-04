f87081f8848cd3fd1e6854da9a8780c1c2a04e41
csdoker <csdokerchen@yunify.com>
2021-03-04 16:42:16 +0800

    feat: 歌手-横向分类组件开发

--- src/application/Singers/index.js
+++ src/application/Singers/index.js
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Horizen from '../../baseUI/horizen-item'
+import { categoryTypes, alphaTypes } from '../../api/config'
+import { NavContainer } from './style'
 
-function Home(props) {
-  return <div> Singers </div>
+function Singers() {
+  let [category, setCategory] = useState('')
+  let [alpha, setAlpha] = useState('')
+
+  let handleUpdateAlpha = (val) => {
+    setAlpha(val)
+  }
+
+  let handleUpdateCatetory = (val) => {
+    setCategory(val)
+  }
+
+  return (
+    <NavContainer>
+      <Horizen
+        list={categoryTypes}
+        title={'分类 (默认热门):'}
+        handleClick={handleUpdateCatetory}
+        oldVal={category}
+      ></Horizen>
+      <Horizen
+        list={alphaTypes}
+        title={'首字母:'}
+        handleClick={(val) => handleUpdateAlpha(val)}
+        oldVal={alpha}
+      ></Horizen>
+    </NavContainer>
+  )
 }
 
-export default React.memo(Home)
+export default React.memo(Singers)
