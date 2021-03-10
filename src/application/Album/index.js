b58901db0f113338f0122a00aa6ab4072bddabd2
csdoker <csdokerchen@yunify.com>
2021-03-10 15:39:57 +0800

    feat: 完成歌单详情切换页面的过渡动画

--- src/application/Album/index.js
+++ src/application/Album/index.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container } from './style'
+import { CSSTransition } from 'react-transition-group'
+import Header from './../../baseUI/header'
 
 function Album(props) {
-  return <Container></Container>
+  const [showStatus, setShowStatus] = useState(true)
+  const handleBack = () => {
+    setShowStatus(false)
+  }
+
+  return (
+    <CSSTransition
+      in={showStatus}
+      timeout={300}
+      classNames='fly'
+      appear={true}
+      unmountOnExit
+      onExited={props.history.goBack}
+    >
+      <Container>
+        <Header title={'返回'} handleClick={handleBack}></Header>
+      </Container>
+    </CSSTransition>
+  )
 }
 
-export default Album
+export default React.memo(Album)
